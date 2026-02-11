a97b474cd86b89397b11c64e28396d4a6c15719f
developer-madhan <madhankumarjothi@gmail.com>
2026-02-11 23:38:52 +0530

    Refactor Program class to Car with fields and output
    Replaced the Program class with a Car class containing color and maxSpeed fields. Updated Main to instantiate Car and print its field values instead of "Hello, World!".

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -1,10 +1,15 @@
 ﻿namespace ConsoleApp2
 {
-    internal class Program
+    class Car
     {
+        string color = "red";
+        int maxSpeed = 200;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Car myObj = new Car();
+            Console.WriteLine(myObj.color);
+            Console.WriteLine(myObj.maxSpeed);
         }
     }
 }
