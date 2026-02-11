f0bb89fc0dc65b5ea72f02af8e653957c0d854c4
developer-madhan <madhankumarjothi@gmail.com>
2026-02-11 23:49:55 +0530

    Update Car class fields and Main method usage
    Removed maxSpeed field and added year to Car class.
    Main now creates Ford and Opel cars, sets model, color, year,
    and prints their models instead of color and maxSpeed.

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -2,16 +2,24 @@
 {
     class Car
     {
+        string model;
         string color;
-        int maxSpeed;
+        int year;
 
         static void Main(string[] args)
         {
-            Car myObj = new Car();
-            myObj.color = "red";
-            myObj.maxSpeed = 200;
-            Console.WriteLine(myObj.color);
-            Console.WriteLine(myObj.maxSpeed);
+            Car Ford = new Car();
+            Ford.model = "Mustang";
+            Ford.color = "red";
+            Ford.year = 1969;
+
+            Car Opel = new Car();
+            Opel.model = "Astra";
+            Opel.color = "white";
+            Opel.year = 2005;
+
+            Console.WriteLine(Ford.model);
+            Console.WriteLine(Opel.model);
         }
     }
 }
