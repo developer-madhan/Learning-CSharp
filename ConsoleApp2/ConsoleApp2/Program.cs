4cc0db0af34116b7cd3440f4001402c99e819704
developer-madhan <madhankumarjothi@gmail.com>
2026-02-11 23:58:57 +0530

    Move Car class to Program2.cs and update Main logic
    Refactored Car class into Program2.cs with public fields and a method. Updated Program.cs Main method to create and initialize two Car objects and print their model names. Removed old Car class definition from Program.cs.

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -1,18 +1,21 @@
 ﻿namespace ConsoleApp2
 {
-    class Car
+    class Program
     {
-        string color;                 // field
-        int maxSpeed;                 // field
-        public void fullThrottle()    // method
-        {
-            Console.WriteLine("The car is going as fast as it can!");
-        }
-
         static void Main(string[] args)
         {
-            Car myObj = new Car();
-            myObj.fullThrottle();  // Call the method
+            Car Ford = new Car();
+            Ford.model = "Mustang";
+            Ford.color = "red";
+            Ford.year = 1969;
+
+            Car Opel = new Car();
+            Opel.model = "Astra";
+            Opel.color = "white";
+            Opel.year = 2005;
+
+            Console.WriteLine(Ford.model);
+            Console.WriteLine(Opel.model);
         }
     }
 
---
+++ ConsoleApp2/ConsoleApp2/Program2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    class Car
+    {
+        public string model;
+        public string color;
+        public int year;
+        public void fullThrottle()
+        {
+            Console.WriteLine("The car is going as fast as it can!");
+        }
+    }
+}
