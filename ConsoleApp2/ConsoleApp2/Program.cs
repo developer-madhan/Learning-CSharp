5a23126dcf8f994f8abebcbfc119c5c8cc604b69
developer-madhan <madhankumarjothi@gmail.com>
2026-02-12 00:41:22 +0530

    Replace Car class with Person class and update Main
    Replaced the Car class with a new Person class featuring a Name property. Updated Main to create and display a Person object instead of a Car. Commented out the old Car class and its usage for reference.

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -1,25 +1,45 @@
 ﻿namespace ConsoleApp2
 {
-    class Car
+    class Person
     {
-        public string model;
-        public string color;
-        public int year;
-
-        // Create a class constructor with multiple parameters
-        public Car(string modelName, string modelColor, int modelYear)
+        private string name; // field
+        public string Name   // property
         {
-            model = modelName;
-            color = modelColor;
-            year = modelYear;
-        }
-
-        static void Main(string[] args)
-        {
-            Car Ford = new Car("Mustang", "Red", 1969);
-            Console.WriteLine(Ford.color + " " + Ford.year + " " + Ford.model);
+            get { return name; }
+            set { name = value; }
         }
     }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person myObj = new Person();
+            myObj.Name = "Liam";
+            Console.WriteLine(myObj.Name);
+        }
+    }
+
+    //class Car
+    //{
+    //    public string model;
+    //    public string color;
+    //    public int year;
+
+    //    // Create a class constructor with multiple parameters
+    //    public Car(string modelName, string modelColor, int modelYear)
+    //    {
+    //        model = modelName;
+    //        color = modelColor;
+    //        year = modelYear;
+    //    }
+
+    //    static void Main(string[] args)
+    //    {
+    //        Car Ford = new Car("Mustang", "Red", 1969);
+    //        Console.WriteLine(Ford.color + " " + Ford.year + " " + Ford.model);
+    //    }
+    //}
 
 
     // Outputs Red 1969 Mustang
