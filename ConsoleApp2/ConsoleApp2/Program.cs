472adb81f7bc3d71296cfcd4a9735020b131feee
developer-madhan <madhankumarjothi@gmail.com>
2026-02-12 00:13:20 +0530

    Move and simplify Car class; update usage in Program.cs
    Moved Car class from Program2.cs to Program.cs and simplified it to include only a model field with a constructor. Updated Main to create and print a single Car object. Commented out previous code for additional properties and objects. Commented out Car class in Program2.cs.

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -1,23 +1,44 @@
 ﻿namespace ConsoleApp2
 {
-    class Program
+    // Create a Car class
+    class Car
     {
+        public string model;  // Create a field
+
+        // Create a class constructor for the Car class
+        public Car()
+        {
+            model = "Mustang"; // Set the initial value for model
+        }
+
         static void Main(string[] args)
         {
-            Car Ford = new Car();
-            Ford.model = "Mustang";
-            Ford.color = "red";
-            Ford.year = 1969;
-
-            Car Opel = new Car();
-            Opel.model = "Astra";
-            Opel.color = "white";
-            Opel.year = 2005;
-
-            Console.WriteLine(Ford.model);
-            Console.WriteLine(Opel.model);
+            Car Ford = new Car();  // Create an object of the Car Class (this will call the constructor)
+            Console.WriteLine(Ford.model);  // Print the value of model
         }
     }
+
+    // Outputs "Mustang"
+
+
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        Car Ford = new Car();
+    //        Ford.model = "Mustang";
+    //        Ford.color = "red";
+    //        Ford.year = 1969;
+
+    //        Car Opel = new Car();
+    //        Opel.model = "Astra";
+    //        Opel.color = "white";
+    //        Opel.year = 2005;
+
+    //        Console.WriteLine(Ford.model);
+    //        Console.WriteLine(Opel.model);
+    //    }
+    //}
 
     //class Car
     //{
--- ConsoleApp2/ConsoleApp2/Program2.cs
+++ ConsoleApp2/ConsoleApp2/Program2.cs
@@ -4,14 +4,14 @@ using System.Text;
 
 namespace ConsoleApp2
 {
-    class Car
-    {
-        public string model;
-        public string color;
-        public int year;
-        public void fullThrottle()
-        {
-            Console.WriteLine("The car is going as fast as it can!");
-        }
-    }
+    //class Car
+    //{
+    //    public string model;
+    //    public string color;
+    //    public int year;
+    //    public void fullThrottle()
+    //    {
+    //        Console.WriteLine("The car is going as fast as it can!");
+    //    }
+    //}
 }
