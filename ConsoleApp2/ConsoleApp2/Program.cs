a6063fef38249e19661a76905a1fe6cf4ab5b654
developer-madhan <madhankumarjothi@gmail.com>
2026-02-12 00:55:54 +0530

    Add commented example for Person class and usage
    Added commented-out code showing the Person class with a Name property and a sample Program class that demonstrates object creation and property access. No functional changes were made; additions are for reference and illustration.

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -20,6 +20,26 @@
         }
     }
 
+    //class Person
+    //{
+    //    private string name; // field
+    //    public string Name   // property
+    //    {
+    //        get { return name; }
+    //        set { name = value; }
+    //    }
+    //}
+
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        Person myObj = new Person();
+    //        myObj.Name = "Liam";
+    //        Console.WriteLine(myObj.Name);
+    //    }
+    //}
+
     //class Car
     //{
     //    public string model;
