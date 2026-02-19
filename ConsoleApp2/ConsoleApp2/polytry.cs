3abb0fe057c7b5a21287ed1b459bead8e1f87391
developer-madhan <madhankumarjothi@gmail.com>
2026-02-19 12:21:28 +0530

    try polymorpism on run time

--- ConsoleApp2/ConsoleApp2/polytry.cs
+++ ConsoleApp2/ConsoleApp2/polytry.cs
@@ -5,9 +5,33 @@ using System.Text;
 namespace ConsoleApp2
 {
     class welcomeMessage {
-        public void displayMessage()
+        public virtual void paymode(int amount)
         {
             Console.WriteLine("Welcome to the Madhan Payment System");
+        }
+    }
+
+    class card : welcomeMessage
+    {
+        public override void paymode(int amount)
+        {
+            Console.WriteLine("This is the card payment method", amount);
+        }
+    }
+
+    class upi: welcomeMessage
+    {
+        public override void paymode(int amount)
+        {
+            Console.WriteLine("this is the upi payment method", amount);
+        }
+    }
+
+    class cash : welcomeMessage
+    {
+        public override void paymode(int amount)
+        {
+            Console.WriteLine("this is the cash payment method", amount);
         }
     }
 
@@ -15,9 +39,11 @@ namespace ConsoleApp2
 
     class program { 
         static void Main(string[] args) {
-            welcomeMessage message1 = new welcomeMessage();
+            welcomeMessage obj1 = new cash();
+            obj1.paymode(1000);
+            //welcomeMessage message1 = new welcomeMessage();
 
-            message1.displayMessage();
+            //message1.displayMessage();
         }
     }
 }
