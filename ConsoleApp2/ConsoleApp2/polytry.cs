d4cc06a0c22dceba4a89554892737599a0312405
developer-madhan <madhankumarjothi@gmail.com>
2026-02-20 11:23:45 +0530

    try runtime poly code

--- ConsoleApp2/ConsoleApp2/polytry.cs
+++ ConsoleApp2/ConsoleApp2/polytry.cs
@@ -5,7 +5,7 @@ using System.Text;
 namespace ConsoleApp2
 {
     class welcomeMessage {
-        public virtual void paymode(int amount)
+        public virtual void paymode(double amount)
         {
             Console.WriteLine("Welcome to the Madhan Payment System");
         }
@@ -13,7 +13,7 @@ namespace ConsoleApp2
 
     class card : welcomeMessage
     {
-        public override void paymode(int amount)
+        public override void paymode(double amount)
         {
             Console.WriteLine("This is the card payment method", amount);
         }
@@ -21,7 +21,7 @@ namespace ConsoleApp2
 
     class upi: welcomeMessage
     {
-        public override void paymode(int amount)
+        public override void paymode(double amount)
         {
             Console.WriteLine("this is the upi payment method", amount);
         }
@@ -29,7 +29,7 @@ namespace ConsoleApp2
 
     class cash : welcomeMessage
     {
-        public override void paymode(int amount)
+        public override void paymode(double amount)
         {
             Console.WriteLine("this is the cash payment method", amount);
         }
@@ -40,7 +40,37 @@ namespace ConsoleApp2
     class program { 
         static void Main(string[] args) {
             welcomeMessage obj1 = new cash();
-            obj1.paymode(1000);
+            //obj1.paymode(1000);
+
+            Console.Write("Enter amount: ");
+            double amount = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("\nChoose Payment Mode:");
+            Console.WriteLine("1. Card");
+            Console.WriteLine("2. UPI");
+            Console.WriteLine("3. Cash");
+
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    obj1 = new card();
+                    break;
+                case 2:
+                    obj1 = new upi();
+                    break;
+                case 3:
+                    obj1 = new cash();
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice");
+                    return;
+            }
+
+            obj1.paymode(amount);
+
+
             //welcomeMessage message1 = new welcomeMessage();
 
             //message1.displayMessage();
