09d897cf6b6e48ea49b99f6c4aeceed217cddcfa
developer-madhan <madhankumarjothi@gmail.com>
2026-02-21 11:12:41 +0530

    start for the abstract

---
+++ ConsoleApp2/ConsoleApp2/adstract.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("this is the abstract class example");
+        }
+    }
+}
--- ConsoleApp2/ConsoleApp2/polytry.cs
+++ ConsoleApp2/ConsoleApp2/polytry.cs
@@ -4,76 +4,76 @@ using System.Text;
 
 namespace ConsoleApp2
 {
-    class welcomeMessage {
-        public virtual void paymode(double amount)
-        {
-            Console.WriteLine("Welcome to the Madhan Payment System");
-        }
-    }
+    //class welcomeMessage {
+    //    public virtual void paymode(double amount)
+    //    {
+    //        Console.WriteLine("Welcome to the Madhan Payment System");
+    //    }
+    //}
 
-    class card : welcomeMessage
-    {
-        public override void paymode(double amount)
-        {
-            Console.WriteLine("This is the card payment method", amount);
-        }
-    }
+    //class card : welcomeMessage
+    //{
+    //    public override void paymode(double amount)
+    //    {
+    //        Console.WriteLine("This is the card payment method", amount);
+    //    }
+    //}
 
-    class upi: welcomeMessage
-    {
-        public override void paymode(double amount)
-        {
-            Console.WriteLine("this is the upi payment method", amount);
-        }
-    }
+    //class upi: welcomeMessage
+    //{
+    //    public override void paymode(double amount)
+    //    {
+    //        Console.WriteLine("this is the upi payment method", amount);
+    //    }
+    //}
 
-    class cash : welcomeMessage
-    {
-        public override void paymode(double amount)
-        {
-            Console.WriteLine("this is the cash payment method", amount);
-        }
-    }
-
-
-
-    class program { 
-        static void Main(string[] args) {
-            welcomeMessage obj1 = new cash();
-            //obj1.paymode(1000);
-
-            Console.Write("Enter amount: ");
-            double amount = Convert.ToDouble(Console.ReadLine());
-
-            Console.WriteLine("\nChoose Payment Mode:");
-            Console.WriteLine("1. Card");
-            Console.WriteLine("2. UPI");
-            Console.WriteLine("3. Cash");
-
-            int choice = Convert.ToInt32(Console.ReadLine());
-
-            switch (choice)
-            {
-                case 1:
-                    obj1 = new card();
-                    break;
-                case 2:
-                    obj1 = new upi();
-                    break;
-                case 3:
-                    obj1 = new cash();
-                    break;
-                default:
-                    Console.WriteLine("Invalid choice");
-                    return;
-            }
-
-            obj1.paymode(amount);
+    //class cash : welcomeMessage
+    //{
+    //    public override void paymode(double amount)
+    //    {
+    //        Console.WriteLine("this is the cash payment method", amount);
+    //    }
+    //}
 
 
-            //welcomeMessage message1 = new welcomeMessage();
 
-            //message1.displayMessage();
-        }
-    }
+    //class program { 
+    //    static void Main(string[] args) {
+    //        welcomeMessage obj1 = new cash();
+    //        //obj1.paymode(1000);
+
+    //        Console.Write("Enter amount: ");
+    //        double amount = Convert.ToDouble(Console.ReadLine());
+
+    //        Console.WriteLine("\nChoose Payment Mode:");
+    //        Console.WriteLine("1. Card");
+    //        Console.WriteLine("2. UPI");
+    //        Console.WriteLine("3. Cash");
+
+    //        int choice = Convert.ToInt32(Console.ReadLine());
+
+    //        switch (choice)
+    //        {
+    //            case 1:
+    //                obj1 = new card();
+    //                break;
+    //            case 2:
+    //                obj1 = new upi();
+    //                break;
+    //            case 3:
+    //                obj1 = new cash();
+    //                break;
+    //            default:
+    //                Console.WriteLine("Invalid choice");
+    //                return;
+    //        }
+
+    //        obj1.paymode(amount);
+
+
+    //        //welcomeMessage message1 = new welcomeMessage();
+
+    //        //message1.displayMessage();
+    //    }
+    //}
 }
