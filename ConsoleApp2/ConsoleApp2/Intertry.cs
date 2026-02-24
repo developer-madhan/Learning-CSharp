35ae5770505e95390fd25c77ab099cb9c69b3e9e
developer-madhan <madhankumarjothi@gmail.com>
2026-02-24 12:43:16 +0530

    create auth flow on c# to 3 attempts

---
+++ ConsoleApp2/ConsoleApp2/Intertry.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int attempts = 0;
+            int enteredPin = 0;
+            bool loginSuccess = false;
+
+            while (attempts < 3) 
+            {
+                Console.WriteLine("Enter Your PIN:");
+                string? inputPin = Console.ReadLine();
+
+                if (!int.TryParse(inputPin, out enteredPin)) 
+                {
+                    Console.WriteLine("Invalid Input! Enter numbers only.");
+                    continue;
+                }
+                if (enteredPin == 1234)
+                {
+                    loginSuccess = true;
+                    Console.WriteLine("Login Successful!");
+                    break;
+                }
+                else
+                {
+                    attempts++;
+                    Console.WriteLine("Wrong PIN! Attempts Left: " + (3 - attempts));
+                }
+            }
+            if (!loginSuccess)
+            {
+                Console.WriteLine("Account Locked. Try Again Later.");
+                return;
+            }
+        }
+    }
+}
--- ConsoleApp2/ConsoleApp2/adstract.cs
+++ ConsoleApp2/ConsoleApp2/adstract.cs
@@ -5,89 +5,89 @@ using System.Text;
 namespace ConsoleApp2
 {
   
-        abstract class ATM
-        {
-            protected double balance = 10000;
-            public abstract void Withdraw(double amount);
-            public abstract void Deposit(double amount);
-            public abstract void CheckBalance();
-        }
+    //    abstract class ATM
+    //    {
+    //        protected double balance = 10000;
+    //        public abstract void Withdraw(double amount);
+    //        public abstract void Deposit(double amount);
+    //        public abstract void CheckBalance();
+    //    }
 
-        class MyATM : ATM
-        {
-            public override void Withdraw(double amount)
-            {
-                //Console.WriteLine("Withdraw method called");
-                if (amount <= balance)
-                {
-                    balance -= amount;
-                    Console.WriteLine("Withdraw Successful");
-                }
-                else
-                {
-                    Console.WriteLine("Insufficient Balance");
-                }
-            }
+    //    class MyATM : ATM
+    //    {
+    //        public override void Withdraw(double amount)
+    //        {
+    //            //Console.WriteLine("Withdraw method called");
+    //            if (amount <= balance)
+    //            {
+    //                balance -= amount;
+    //                Console.WriteLine("Withdraw Successful");
+    //            }
+    //            else
+    //            {
+    //                Console.WriteLine("Insufficient Balance");
+    //            }
+    //        }
 
-            public override void Deposit(double amount)
-            {
-                //Console.WriteLine("Deposit method called");
-                balance += amount;
-                Console.WriteLine("Deposit Successful");
-            }
+    //        public override void Deposit(double amount)
+    //        {
+    //            //Console.WriteLine("Deposit method called");
+    //            balance += amount;
+    //            Console.WriteLine("Deposit Successful");
+    //        }
 
-            public override void CheckBalance()
-            {
-                //Console.WriteLine("Check Balance method called");
-                Console.WriteLine("Balance: " + balance);
-            }
-        }
+    //        public override void CheckBalance()
+    //        {
+    //            //Console.WriteLine("Check Balance method called");
+    //            Console.WriteLine("Balance: " + balance);
+    //        }
+    //    }
 
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            //Console.WriteLine("this is the abstract class example");
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        //Console.WriteLine("this is the abstract class example");
 
-            ATM atm = new MyATM();
+    //        ATM atm = new MyATM();
 
-            while (true)
-            {
-                Console.WriteLine("\n === ATM MENU ===");
-                Console.WriteLine("1.Withdraw");
-                Console.WriteLine("2.Deposit");
-                Console.WriteLine("3.Check Balance");
-                Console.WriteLine("4.Exit");
+    //        while (true)
+    //        {
+    //            Console.WriteLine("\n === ATM MENU ===");
+    //            Console.WriteLine("1.Withdraw");
+    //            Console.WriteLine("2.Deposit");
+    //            Console.WriteLine("3.Check Balance");
+    //            Console.WriteLine("4.Exit");
 
-                Console.Write("Enter your choice: ");
-                int choice = Convert.ToInt32(Console.ReadLine());
+    //            Console.Write("Enter your choice: ");
+    //            int choice = Convert.ToInt32(Console.ReadLine());
 
-                switch(choice)
-                {
-                    case 1:
-                        //Console.WriteLine("You have chosen to Withdraw");
-                        Console.Write("Enter amount: ");
-                        double w = Convert.ToDouble(Console.ReadLine());
-                        atm.Withdraw(w);
-                        break;
-                    case 2:
-                        //Console.WriteLine("You have chosen to Deposit");
-                        Console.Write("Enter amount: ");
-                        double d = Convert.ToDouble(Console.ReadLine());
-                        atm.Deposit(d);
-                        break;
-                    case 3:
-                        //Console.WriteLine("You have chosen to Check Balance");
-                        atm.CheckBalance();
-                        break;
-                    case 4:
-                        Console.WriteLine("Exiting the program. Thank you!");
-                        return;
-                    default:
-                        Console.WriteLine("Invalid choice. Please try again.");
-                        break;
-                }
-            }
-        }
-    }
+    //            switch(choice)
+    //            {
+    //                case 1:
+    //                    //Console.WriteLine("You have chosen to Withdraw");
+    //                    Console.Write("Enter amount: ");
+    //                    double w = Convert.ToDouble(Console.ReadLine());
+    //                    atm.Withdraw(w);
+    //                    break;
+    //                case 2:
+    //                    //Console.WriteLine("You have chosen to Deposit");
+    //                    Console.Write("Enter amount: ");
+    //                    double d = Convert.ToDouble(Console.ReadLine());
+    //                    atm.Deposit(d);
+    //                    break;
+    //                case 3:
+    //                    //Console.WriteLine("You have chosen to Check Balance");
+    //                    atm.CheckBalance();
+    //                    break;
+    //                case 4:
+    //                    Console.WriteLine("Exiting the program. Thank you!");
+    //                    return;
+    //                default:
+    //                    Console.WriteLine("Invalid choice. Please try again.");
+    //                    break;
+    //            }
+    //        }
+    //    }
+    //}
 }
